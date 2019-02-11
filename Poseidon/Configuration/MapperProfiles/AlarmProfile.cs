acbce2363a137ef05eda25760453753a956dc7b3
AlexisMtr <a.martinier@gmail.com>
2019-02-11 16:13:18 +0100

    Extract PaginatedMapping outside the Profile

--- Poseidon/Configuration/MapperProfiles/AlarmProfile.cs
+++ Poseidon/Configuration/MapperProfiles/AlarmProfile.cs
@@ -13,17 +13,8 @@ namespace Poseidon.Configuration.MapperProfiles
                 .ForMember(d => d.OccuredAt, opt => opt.MapFrom(s => s.DateTime))
                 .ForMember(d => d.AlarmType, opt => opt.MapFrom(s => s.AlarmType.ToString()))
                 .ForMember(d => d.AcknowledgmentUri, opt => opt.ResolveUsing<RestApiResolver>());
-            CreateMap<PaginatedElement<Alarm>, PaginatedDto<AlarmDto>>()
-                .ForMember(d => d.NextPageUrl, opt =>
-                {
-                    opt.PreCondition(e => e.PageCount > 1);
-                    opt.ResolveUsing<RestApiResolver, string>(e => opt.DestinationMember.Name);
-                })
-                .ForMember(d => d.PreviousPageUrl, opt =>
-                 {
-                     opt.PreCondition(e => e.PageCount > 1);
-                     opt.ResolveUsing<RestApiResolver, string>(e => opt.DestinationMember.Name);
-                 });
+
+            this.CreatePaginatedMap<Alarm, AlarmDto>();
         }
     }
 }
---
+++ Poseidon/Configuration/MapperProfiles/ProfileHelper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Poseidon.Dtos;
+using Poseidon.Models;
+
+namespace Poseidon.Configuration.MapperProfiles
+{
+    public static class ProfileHelper
+    {
+        public static void CreatePaginatedMap<TSource, TDestination>(this Profile profile)
+        {
+            profile.CreateMap<PaginatedElement<TSource>, PaginatedDto<TDestination>>()
+                .ForMember(d => d.NextPageUrl, opt =>
+                {
+                    opt.PreCondition(e => e.PageCount > 1);
+                    opt.ResolveUsing<RestApiResolver, string>(e => opt.DestinationMember.Name);
+                })
+                .ForMember(d => d.PreviousPageUrl, opt =>
+                {
+                    opt.PreCondition(e => e.PageCount > 1);
+                    opt.ResolveUsing<RestApiResolver, string>(e => opt.DestinationMember.Name);
+                });
+        }
+    }
+}
